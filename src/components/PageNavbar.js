bf9ff46324d724dac69546936945b153bc84c6d3
Baharin A. Jodat <25382501+baharin@users.noreply.github.com>
2023-07-17 12:44:53 -0400

    Add files via upload

--- src/components/PageNavbar.js
+++ src/components/PageNavbar.js
@@ -3,8 +3,32 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import i18n from '../i18n';
+import { useTranslation } from 'react-i18next';
+import LangContext from '../LangContext'
+import { useContext } from 'react';
+
+const langs = {
+  English: {title: 'English'},
+  French: {title: 'French'}
+}
+
 
 function PageNavbar() {
+
+  const {t} = useTranslation();
+
+  const { lang } = useContext(LangContext);
+
+  function changeLang (l) {
+    if (lang !== l) {
+      i18n.changeLanguage(l);
+      
+    }
+  }
+
+
+
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
@@ -12,10 +36,17 @@ function PageNavbar() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="/Project2">Home</Nav.Link>
-            <Nav.Link href="/Project2/Services">Our Tutors</Nav.Link>
-            <Nav.Link href='/Project2/StudyGroups'>Study Groups</Nav.Link>
-            <Nav.Link href="/Project2/Aboutus">About us</Nav.Link>
+            <Nav.Link href="/Project2">{t('Navbar.Home')}</Nav.Link>
+            <Nav.Link href="/Project2/Services">{t('Navbar.OurTutors')}</Nav.Link>
+            <Nav.Link href='/Project2/StudyGroups'>{t('Navbar.StudyGroups')}</Nav.Link>
+            <Nav.Link href="/Project2/Aboutus">{t('Navbar.AboutUs')}</Nav.Link>
+            <NavDropdown title={lang} id="basic-nav-dropdown">
+              {Object.keys(langs).map((lang) => (
+                <NavDropdown.Item style={{ fontWeight: i18n.resolvedLanguage === lang ? 'bold' : 'normal' }} onClick={() => changeLang(lang)}>
+                  {langs[lang].title}
+                </NavDropdown.Item>
+              ))}
+            </NavDropdown>
           </Nav>
         </Navbar.Collapse>
       </Container>
@@ -24,3 +55,4 @@ function PageNavbar() {
 }
 
 export default PageNavbar;
+
